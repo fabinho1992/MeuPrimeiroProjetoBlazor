ac09fdf9e308ee31c7ba8233bb4073dc1e3b1bf1
fabinho1992 <f.santosdev1992@gmail.com>
2024-05-03 08:40:35 -0300

    Iniciando Projeto

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -15,11 +15,15 @@ public class ScreenSoundContext: DbContext
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Data Source=DESKTOP-7JJ4VOJ\\SQLEXPRESS;Initial Catalog=DbScreenSound;Integrated Security=True;TrustServerCertificate=True";
 
     public ScreenSoundContext(DbContextOptions options) : base(options)
     {
 
+    }
+
+    public ScreenSoundContext()
+    {
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
